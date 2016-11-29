9ffc4dc9d8ac5c087f561b6a2b32afee1b12eb12
AaronLuna <aaronluna222@gmail.com>
2016-11-28 19:19:35 -0600

    mas nodejs :)

--- NodeJs/MD5/md5.js
+++ NodeJs/MD5/md5.js
@@ -11,22 +11,22 @@ var connection = mysql.createConnection({
     database: 'tokens',
     multipleStatements: true
 });
-var ipService ="127.0.0.1";
-var puerto=1337;
+var ipService = "127.0.0.1";
+var puerto = 1337;
 
 /*ATOM */
 
 
 
 /********* Metodo que levanat el servicio ************/
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
 
     var metodo = req.method;
     var urlRequest = req.url;
     var ipOrigen = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var key = req.headers['key'];
     var clienId = req.headers['client-id'];
-    var oauthToken = md5(key+clienId+ipOrigen);
+    var oauthToken = md5(key + clienId + ipOrigen);
 
     log_luci(urlRequest);
     log_luci(metodo);
@@ -36,21 +36,25 @@ http.createServer(function (req, res) {
     log_luci(oauthToken);
 
 
-    connection.query('SELECT * FROM target WHERE auth_key=\''+oauthToken+'\'' , function (err, result, fields) {
-        if (err){
+    connection.query('SELECT * FROM target WHERE auth_key=\'' + oauthToken + '\'', function(err, result, fields) {
+        if (err) {
 
-            res.writeHead(400, {'Content-Type': 'application/json'});
+            res.writeHead(400, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"resp\": \"400\",\"desc\": \"Problemas de conexion\"}');
             throw err;
             log_luci(err);
-        }else{
+        } else {
 
             if (result.length > 0) {
                 metodos(metodo, urlRequest, res);
                 log_luci(" ");
                 log_luci("-------------------------------------");
-            }else{
-                res.writeHead(404, {'Content-Type': 'application/json'});
+            } else {
+                res.writeHead(404, {
+                    'Content-Type': 'application/json'
+                });
                 res.end('{\"resp\": \"400\",\"desc\": \"Auth not correct\"}');
                 log_luci(" ");
                 log_luci("-------------------------------------");
@@ -65,14 +69,14 @@ http.createServer(function (req, res) {
 
 /****** Funciones dentro del API ***********/
 
-function log_luci(description){
+function log_luci(description) {
 
     var currentDate = new Date();
-    console.log("->"+description+" - "+currentDate);
+    console.log("->" + description + " - " + currentDate);
 }
 
-function metodos(metodo, urlRequest, res){
-    if(urlRequest.indexOf("/luci/reus/phone") !=-1){
+function metodos(metodo, urlRequest, res) {
+    if (urlRequest.indexOf("/luci/reus/phone") != -1) {
 
         if (metodo == 'GET') {
 
@@ -94,29 +98,37 @@ function metodos(metodo, urlRequest, res){
               }
             });*/
 
-            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55############\"}');
 
 
 
 
-        }else if(metodo == 'POST'){
-            res.writeHead(200, {'Content-Type': 'application/json'});
+        } else if (metodo == 'POST') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55############\"}');
 
 
-        }else if(metodo == 'DELETE'){
-            res.writeHead(200, {'Content-Type': 'application/json'});
+        } else if (metodo == 'DELETE') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55############\"}');
 
 
-        }else{
-            res.writeHead(404, {'Content-Type': 'text/plain'});
+        } else {
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
             res.end('\t\t\t\t\t\tProtocolo RESTFULL no valido\n');
         }
 
 
-    }else if (urlRequest.indexOf("/luci/callinprogress/phone") !=-1) {
+    } else if (urlRequest.indexOf("/luci/callinprogress/phone") != -1) {
 
         if (metodo == 'GET') {
 
@@ -138,18 +150,22 @@ function metodos(metodo, urlRequest, res){
               }
             });*/
 
-            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"inprogress\": \"T\", \"extension\":\"2001343455\"}');
 
 
 
 
-        }else{
-            res.writeHead(404, {'Content-Type': 'text/plain'});
+        } else {
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
             res.end('\t\t\t\t\t\tProtocolo RESTFULL no valido\n');
         }
 
-    }else if (urlRequest.indexOf("/luci/setFolio") !=-1) {
+    } else if (urlRequest.indexOf("/luci/setFolio") != -1) {
         if (metodo == 'POST') {
 
             /*request('http://69.195.197.50:4045/callInProcess?do={"ag":"'+numeroAgencia+'","mc":"'+numerMiniCall+'","phone":"'+parametros.numero+'"}', function (error, response, body) {
@@ -170,18 +186,22 @@ function metodos(metodo, urlRequest, res){
               }
             });*/
 
-            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55###########\"}');
 
 
 
 
-        }else{
-            res.writeHead(404, {'Content-Type': 'text/plain'});
+        } else {
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
             res.end('\t\t\t\t\t\tProtocolo RESTFULL no valido\n');
         }
 
-    }else if (urlRequest.indexOf("/luci/whitelist/trusted/phone") !=-1) {
+    } else if (urlRequest.indexOf("/luci/whitelist/trusted/phone") != -1) {
 
         if (metodo == 'GET') {
 
@@ -203,33 +223,43 @@ function metodos(metodo, urlRequest, res){
               }
             });*/
 
-            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55############\"}');
 
 
 
 
-        }else if(metodo == 'POST'){
-            res.writeHead(200, {'Content-Type': 'application/json'});
+        } else if (metodo == 'POST') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55############\"}');
 
 
-        }else if(metodo == 'DELETE'){
-            res.writeHead(200, {'Content-Type': 'application/json'});
+        } else if (metodo == 'DELETE') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
             res.end('{\"response\": \"200\",\"description\": \"Success\", \"phone\":\"55############\"}');
 
 
-        }else{
-            res.writeHead(404, {'Content-Type': 'text/plain'});
+        } else {
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
             res.end('\t\t\t\t\t\tProtocolo RESTFULL no valido\n');
         }
 
-    }else{
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+    } else {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
         res.end('\t\t\t\t\t\tMetodo NO valido dentro del API.\n');
     }
 }
 
 
 
-console.log('Server Luci-API running at '+ipService+' on port '+puerto);
+console.log('Server Luci-API running at ' + ipService + ' on port ' + puerto);
