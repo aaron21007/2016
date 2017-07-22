4c21013211f02b691c096c5a4bb13dcafad65645
Aaron Luna <aaron.luna@directo.com>
2017-07-21 23:43:54 -0500

    cosas

---
+++ python/Network/sniffer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import pcapy
+from impacket.ImpactDecoder import EthDecoder
+
+dev  = "wlp3s0"
+filter = "ip"
+decoder = EthDecoder()
+
+def handle_packet(hdr, data):
+    print decoder.decode(data)
+
+def usage():
+    print sys.argv[0]+" -i <dev> -f <pcap_filter>"
+    sys.exit(1)
+
+try:
+    cmd_opts = "f:i"
+    opts, arg = getopt.getopt(sys.argv[1:], cmd_opts)
+except getopt.GetoptError:
+    usage()
+
+for opt in opts:
+    if opt[0] == "-f":
+        filter=opt[1]
+    elif opt[0] == "-i":
+        dev = opt[1]
+    else:
+        usage()
+
+pcap = pcapy.open_live(dev, 1500, 0, 1000)
+
+pcap.setfilter(filter)
+
+pcap.loop(0, handle_packet)
