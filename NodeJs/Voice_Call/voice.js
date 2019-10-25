3f348f13e48e99ea604530167257f35392faa9f2
Aaron Luna <aaronluna222@gmail.com>
2019-10-25 13:22:06 -0500

    vergas

--- NodeJs/Voice_Call/voice.js
+++ NodeJs/Voice_Call/voice.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC62cab6432c5baec1fbd6e6f0ca4fbfe0';
-const authToken = '73e947b8ea2d1ac8095340da6da7a112';
+const accountSid = '';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 client.calls
