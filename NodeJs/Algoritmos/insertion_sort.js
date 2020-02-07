c5eec0fbba72a373ab099a4f618731f91af6c5ab
Aaron Luna <aaronluna222@gmail.com>
2020-02-07 12:39:09 -0600

    CubeJs

---
+++ NodeJs/Algoritmos/insertion_sort.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+let numeros = [5,7,1,8,3,9,4,2]
+
+for (let i = 0; i < numeros.length; i++) {
+  
+  
+  
+}
--- NodeJs/Mongose/app.js
+++ NodeJs/Mongose/app.js
@@ -1,15 +1,26 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-var Cat = mongoose.model('Cat', { name: String });
-
-var kitty = new Cat({ name: 'Zildjian' });
 
 
-kitty.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
+
+let Schema = mongoose.Schema
+
+const mySchema = new Schema({
+  user: String,
+  message: {type: String, required: true},
+  date: Date,
+})
+
+
+const model  = mongoose.model('Mensajes', mySchema)
+
+mongoose.connect('mongodb://127.0.0.1:27017/Mensajes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+console.log('Ya se conecto este Pedo');
+
+let mensaje = new model({user:"Aaron Luna", message:"Mensaje para mongo DB", date:new Date()})
+mensaje.save()
+
+console.log(model.find());
