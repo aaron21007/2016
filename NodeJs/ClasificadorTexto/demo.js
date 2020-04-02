473d289046655fc41c1b7f4ee6d794485bb16f4c
Aaron Luna <aaronluna222@gmail.com>
2020-04-02 09:07:05 -0600

    pruebas clasificador

--- NodeJs/ClasificadorTexto/demo.js
+++ NodeJs/ClasificadorTexto/demo.js
@@ -9,11 +9,17 @@ classifier.learn('Usa este codigo para recuperar tu usuario Principal Digital.',
 classifier.learn('Disfruta en KFC 8 pzs con pure, ensalada fam y 3 bisquets por $199 ', 'publicidad')
 classifier.learn('Gracias por aceptar La Tarjeta American Express, la visita sera el dia de hoy entre las 11 y 12 hrs', 'publicidad')
 classifier.learn('Hola esto es una prueba', 'otro')
-classifier.learn('prueba', 'otro')
 classifier.learn('Low Server Space', 'alertas')
 classifier.learn('Consorcio representante de Citibanamex', 'cobranza')
 classifier.learn('estudia tu Licenciatura en Linea UNITEC', 'publicidad')
 classifier.learn('Maestria OnLine la UNITEC', 'publicidad')
+classifier.learn('ENVIO GRATIS', 'publicidad')
+classifier.learn('falta de pago', 'cobranza')
+classifier.learn('de su deuda', 'cobranza')
+classifier.learn('pagos vencidos', 'cobranza')
+classifier.learn('prueba', 'otro')
+classifier.learn('mensaje de prueba', 'otro')
+classifier.learn('cambio de nip', 'alertas')
+classifier.learn('alternativas para pagar', 'cobranza')
 
-
-console.log(classifier.categorize("Hola BB como estas esto es una prueba de analisis de texto"));
+console.log(classifier.categorize("Cliente HSBC, hizo un acuerdo con Consorcio Juridico para pagar su credito 1685 el dia de hoy por $60000, llame al 5559995050 o 8002699909 para reportar el pago"));
