3ed1d2e6ca4e6425caa0bcc0a2b54825cfa0ebb9
Aaron Luna <aaronluna222@gmail.com>
2020-04-09 18:46:15 -0500

    fixed untracked files

---
+++ NodeJs/EnviromentsVariables/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+console.log(`${process.env.NODE_ENV }`);
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
