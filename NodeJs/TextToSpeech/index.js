d6d42c438b9d56f93539712bb741cd745091523a
Aaron Luna <aaronluna222@gmail.com>
2020-07-13 16:07:55 -0500

    Audios

--- NodeJs/TextToSpeech/index.js
+++ NodeJs/TextToSpeech/index.js
@@ -7,10 +7,42 @@ const Polly = new AWS.Polly({
   region: 'us-east-2'
 })
 
+/**
+
+ var params = {
+   OutputFormat: json | mp3 | ogg_vorbis | pcm,
+
+   Text: 'STRING_VALUE',
+
+   VoiceId: Aditi | Amy | Astrid | Bianca | Brian | Camila | Carla | Carmen | Celine | Chantal | Conchita | Cristiano | Dora | Emma | Enrique | Ewa | Filiz | Geraint | Giorgio | Gwyneth | Hans | Ines | Ivy | Jacek | Jan | Joanna | Joey | Justin | Karl | Kendra | Kevin | Kimberly | Lea | Liv | Lotte | Lucia | Lupe | Mads | Maja | Marlene | Mathieu | Matthew | Maxim | Mia | Miguel | Mizuki | Naja | Nicole | Penelope | Raveena | Ricardo | Ruben | Russell | Salli | Seoyeon | Takumi | Tatyana | Vicki | Vitoria | Zeina | Zhiyu,
+
+   Engine: standard | neural,
+   LanguageCode: arb | cmn - CN | cy - GB | da - DK | de - DE | en - AU | en - GB | en - GB - WLS | en - IN | en - US | es - ES | es - MX | es - US | fr - CA | fr - FR | is - IS | it - IT | ja - JP | hi - IN | ko - KR | nb - NO | nl - NL | pl - PL | pt - BR | pt - PT | ro - RO | ru - RU | sv - SE | tr - TR,
+   LexiconNames: [
+     'STRING_VALUE',
+  
+   ],
+   SampleRate: 'STRING_VALUE',
+   SpeechMarkTypes: [
+     sentence | ssml | viseme | word,
+
+   ],
+   TextType: ssml | text
+ };
+ polly.synthesizeSpeech(params, function (err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else console.log(data); // successful response
+ });
+ * 
+ */
+
+
 let params = {
-  'Text': 'Hola buenas tardes, muchas gracias por contestar, hablamos de grupo citibanamex, para ofrecerle una tarjeta oro, esta interesado?',
+  'Text': '<speak>Su código de activación es :  <prosody rate="x-slow"> 32 24 16 </prosody>, desea que se le repita el código?, presione 2.</speak>',
   'OutputFormat': 'mp3',
-  'VoiceId': 'Mia'
+  'VoiceId': 'Mia',
+  'SampleRate': "8000",
+  'TextType' : "ssml"
 }
 
 Polly.synthesizeSpeech(params, (err, data) => {
@@ -18,7 +50,7 @@ Polly.synthesizeSpeech(params, (err, data) => {
     console.log(err.code)
   } else if (data) {
     if (data.AudioStream instanceof Buffer) {
-      Fs.writeFile("./speech.mp3", data.AudioStream, function (err) {
+      Fs.writeFile("./activacion.mp3", data.AudioStream, function (err) {
         if (err) {
           return console.log(err)
         }
